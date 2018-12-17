672329a787ad6d09cb37f8240bb8832d7e7f19c1
easonnie <nyixin318@gmail.com>
2018-12-16 23:39:54 -0500

    Model file. :hammer:

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -50,7 +50,41 @@ cd ${DIR_TMP}
 if [[ ! -d dep_packages ]]; then
     mkdir dep_packages
     cd dep_packages
-    wget "https://www.dropbox.com/s/74uc24un1eoqwch/dep_packages.zip?dl=0"
-    mv "dep_packages.zip?dl=0" "dep_packages.zip"
+    wget -O "dep_packages.zip" "https://www.dropbox.com/s/74uc24un1eoqwch/dep_packages.zip?dl=0"
     unzip "dep_packages.zip" && rm "dep_packages.zip" && rm "__MACOSX"
 fi
+
+# download saved model
+cd ${DIR_TMP}
+if [[ ! -d saved_models ]]; then
+    mkdir saved_models
+#    cd saved_models
+#    wget "https://www.dropbox.com/s/74uc24un1eoqwch/dep_packages.zip?dl=0"
+#    mv "dep_packages.zip?dl=0" "dep_packages.zip"
+#    unzip "dep_packages.zip" && rm "dep_packages.zip" && rm "__MACOSX"
+fi
+
+
+cd ${DIR_TMP}/saved_models
+if [[ ! -d saved_nli_m ]]; then
+    wget -O "saved_nli_m.zip" "https://www.dropbox.com/s/rc3zbq8cefhcckg/saved_nli_m.zip?dl=0"
+    unzip "saved_nli_m.zip" && rm "saved_nli_m.zip" && rm "__MACOSX"
+fi
+
+cd ${DIR_TMP}/saved_models
+if [[ ! -d nn_doc_selector ]]; then
+    wget -O "nn_doc_selector.zip" "https://www.dropbox.com/s/hj4zv3k5lzek9yr/nn_doc_selector.zip?dl=0"
+    unzip "nn_doc_selector.zip" && rm "nn_doc_selector.zip" && rm "__MACOSX"
+fi
+
+
+cd ${DIR_TMP}/saved_models
+if [[ ! -d saved_sselector ]]; then
+    wget -O "saved_sselector.zip" "https://www.dropbox.com/s/56tadhfti1zolnz/saved_sselector.zip?dl=0"
+    unzip "saved_sselector.zip" && rm "saved_sselector.zip" && rm "__MACOSX"
+fi
+
+
+# https://www.dropbox.com/s/rc3zbq8cefhcckg/saved_nli_m.zip?dl=0
+# https://www.dropbox.com/s/hj4zv3k5lzek9yr/nn_doc_selector.zip?dl=0
+# https://www.dropbox.com/s/56tadhfti1zolnz/saved_sselector.zip?dl=0
